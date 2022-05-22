f8e98ada18dc26a7722e3f68f87424599aa5eb73
PATHLAVATH <102509181+PATHLAVATH@users.noreply.github.com>
2022-05-22 12:57:34 +0530

    Add files via upload

--- codes/as2.py
+++ codes/as2.py
@@ -1,1 +1,21 @@
+import numpy as np
+
+#creating matrix
+A = np.matrix('[2,4; 3,5]')
+
+#finding transpose of A
+tA = A.T
+
+#adding A and tA
+R = A+tA
+
+#finding tR
+tR = R.T
+
+#to check R is symmetric
+if np.array_equal(R,tR):
+    print("the matrix is symmetric")
+else:
+    print("the matrix is not symmetric")
+    
 
